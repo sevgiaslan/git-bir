d1de3b3bdccd9178d1b0302f6fd4bf6ee02ee2c9
Sevgi Aslan <aslannsevgi@gmail.com>
2017-08-01 10:17:06 +0300

    ilk açıklama

---
+++ src/com/sevgi/Sevgi1.java
@@ -0,0 +1,13 @@
+package com.sevgi;
+
+public class Sevgi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Öðreniyor.");
+		
+		
+	}
+
+}
